2fcac42ad2a3d5025912820c0db3086c54c38520
Luis Gonzalez <gonza462@uwm.edu>
2020-12-02 18:04:08 -0600

    testing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Octokit } from "@octokit/core";
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 const octokit = new Octokit();
@@ -18,20 +18,7 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      hello
     </div>
   );
 }
